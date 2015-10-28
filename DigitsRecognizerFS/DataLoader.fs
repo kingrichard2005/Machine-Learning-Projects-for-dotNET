791740881bc254d7e5ec07bbc8efd85ae904148f
Rich <kingrichard2005@gmail.com>
2015-10-27 22:03:48 -0700

    * moved file handling function into dedicated DataLoader module

---
+++ DigitsRecognizerFS/DataLoader.fs
@@ -0,0 +1,27 @@
+﻿namespace DigitsRecognizer
+open System.IO
+    
+    module DataLoader = 
+        type RawDataSource = { Path:string; includeHeaders: bool }
+        
+        let to2DIntArray (aaInt:int [] []) =
+            let dim1 = Array.length aaInt
+            let dim2 = Array.length aaInt.[0]
+            Array2D.init dim1 dim2 (fun i j -> aaInt.[i].[j]) 
+
+        let CsvStringtoIntArray (csvData:string) =
+            let columns = csvData.Split(',')
+            columns.[0..] |> Array.map int
+
+        let IsFile (_source:string) =
+            match File.Exists(_source) with
+                | true -> true
+                | false -> false
+
+        let CsvFileReader (_source:RawDataSource) =
+            match IsFile(_source.Path) with
+                | true ->
+                    match _source.includeHeaders with
+                        | false -> File.ReadAllLines _source.Path |> Seq.skip 1 |> Seq.toArray // headers
+                        | true -> File.ReadAllLines _source.Path |> Seq.toArray // or no headers
+                | false -> failwith "Invalid File"
--- DigitsRecognizerFS/Script.fsx
+++ DigitsRecognizerFS/Script.fsx
@@ -1,4 +1,6 @@
-﻿open System.IO
+﻿#load "DataLoader.fs"
+open DigitsRecognizer.DataLoader
+open System.IO
 open System
 
 // Define your library scripting code here
@@ -69,32 +71,14 @@ printfn "Euclidean"
 evaluate validationData euclideanClassifierModel
 
 // Sandbox
-type RawDataSource = { Path:string; includeHeaders: bool }
-
-let to2DIntArray (aaInt:int [] []) =
-    let dim1 = Array.length aaInt
-    let dim2 = Array.length aaInt.[0]
-    Array2D.init dim1 dim2 (fun i j -> aaInt.[i].[j]) 
-
-let CsvStringtoIntArray (csvData:string) =
-    let columns = csvData.Split(',')
-    columns.[0..] |> Array.map int
-
-let genericFileReader (_source:RawDataSource) =
-    (* path includeHeaders <- previously distinct input params were rolled up into custom type RawDataSource*) 
-    // playing with pattern matching
-    match _source.includeHeaders with
-    | false -> File.ReadAllLines _source.Path |> Seq.skip 1 |> Seq.toArray
-    | _ -> File.ReadAllLines _source.Path |> Seq.toArray // all else take headers
-
 let ``Read Csv Data To Int Array Then To 2D Array (from pipelined functions)`` (_context:RawDataSource) =
     _context
-    |> genericFileReader
+    |> CsvFileReader
     |> Array.map CsvStringtoIntArray
     |> to2DIntArray
 
 let ``Read Csv Data To Int Array Then To 2D Array (from composed functions)`` = 
-    genericFileReader 
+    CsvFileReader 
     >> Array.map CsvStringtoIntArray 
     >> to2DIntArray
 
