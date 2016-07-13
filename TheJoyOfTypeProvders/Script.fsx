3b87e1da914eebf74953606f09aafbe8aada075b
Rich <kingrichard2005@gmail.com>
2016-07-13 16:41:08 -0700

    * added World Banks typ provider examples

--- TheJoyOfTypeProvders/Script.fsx
+++ TheJoyOfTypeProvders/Script.fsx
@@ -25,4 +25,57 @@ type HardCodedQuestions = JsonProvider<sample>
 let javaQuery = "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=java"
 let javaQuestions = HardCodedQuestions.Load(javaQuery)
 
-// TODO: Building a Minimal DSL to Query Questions
+// See chapter section: Ch 3. - Building a Minimal DSL to Query Questions
+// Listing 3-3. Building a minimal query DSL
+let questionQuery = """https://api.stackexchange.com/2.2/questions?site=stackoverflow"""
+
+let tagged tags query =
+    // join the tags in a ; separated string
+    let joinedTags = tags |> String.concat ";"
+    sprintf "%s&tagged=%s" query joinedTags
+
+let page p query = sprintf "%s&page=%i" query p
+
+let pageSize s query = sprintf "%s&pagesize=%i" query s
+
+let extractQuestions (query:string) = Questions.Load(query).Items
+
+//Listing 3-4. Using our DSL to extract questions tagged C# and F#
+let ``C#`` = "C%23"
+let ``F#`` = "F%23"
+let fsSample =
+    questionQuery
+    |> tagged [``F#``]
+    |> pageSize 100
+    |> extractQuestions
+
+let csSample =
+    questionQuery
+    |> tagged [``C#``]
+    |> pageSize 100
+    |> extractQuestions
+
+//Listing 3-5. Comparing tags by language
+let analyzeTags (qs:Questions.Item seq) =
+    qs
+    |> Seq.collect (fun question -> question.Tags)
+    |> Seq.countBy id
+    |> Seq.filter (fun (_,count) -> count > 2)
+    |> Seq.sortBy (fun (_,count) -> -count)
+    |> Seq.iter (fun (tag,count) -> printfn "%s,%i" tag count)
+
+analyzeTags fsSample
+analyzeTags csSample
+
+// See chapter section: Ch 3. - The World Bank Type Provider
+// Listing 3-6. Using the World Bank type provider
+let wb = WorldBankData.GetDataContext()
+wb.Countries.Japan.CapitalCity
+
+let countries = wb.Countries
+let pop2000 = [ for c in countries -> c.Indicators.``Population, total``.[2000]]
+let pop2010 = [ for c in countries -> c.Indicators.``Population, total``.[2010]]
+
+// TODO: See chapter section: Ch 3. - The R Type Provider
+
+
