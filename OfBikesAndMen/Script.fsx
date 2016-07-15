5a45cad51d44dacda49bf9fa7621f67d64562788
Rich <kingrichard2005@gmail.com>
2016-07-15 12:07:22 -0700

    * added initial example of Spotting Trends with Moving Averages

--- OfBikesAndMen/Script.fsx
+++ OfBikesAndMen/Script.fsx
@@ -9,7 +9,30 @@ let data = dataset.Rows
 
 #load @"FSharp.Charting.0.90.14\FSharp.Charting.fsx"
 open FSharp.Charting
-// Figure 4-2. Day-by-day bicycle usage over time since the first observation
-let all = Chart.Line [ for obs in data -> obs.Cnt ]
 
-// TODO: See chapter section: Ch 4. - Spotting Trends with Moving Averages
+// Figure 4-2. Day-by-day bicycle usage over time since the first observation
+let dataseries = [ for obs in data -> float obs.Cnt ]
+let all = Chart.Line dataseries
+
+// See chapter section: Ch 4. - Spotting Trends with Moving Averages
+
+// This chunks the dataseries 1 .. 10 into blocks of three consecutive values: [[|1; 2; 3|]; [|2; 3; 4|];
+// [|3; 4; 5|]; [|4; 5; 6|]; [|5; 6; 7|]; [|6; 7; 8|]; [|7; 8; 9|]; [|8; 9; 10|]]. From there,
+// generating the moving average (ma) is only a matter of computing the average of each of the chunks:
+let windowedExample =
+    [ for i in 1 .. 10 -> float i ]
+    |> Seq.windowed 3
+    |> Seq.toList
+
+let ma n (dataseries:float list) =
+    dataseries
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
+
+Chart.Combine   [
+    Chart.Line dataseries
+    Chart.Line (ma 7 dataseries)
+    Chart.Line (ma 30 dataseries)]
+
+// TODO See chapter section: Ch 4. - Fitting a Model to the Data
