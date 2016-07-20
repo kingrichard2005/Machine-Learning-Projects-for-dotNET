df23cf2045e786930dd0b0104b9c159425b76fa5
Rich <kingrichard2005@gmail.com>
2016-07-20 12:10:00 -0700

    * added data sample and initial processing logic

---
+++ Chapter5/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Chapter5.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Chapter5")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Chapter5")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("375e69f8-048e-4635-9051-df3edd333219")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Chapter5/Script.fsx
@@ -0,0 +1,33 @@
+// Listing 5-1. Reading the dataset in memory
+open System
+open System.IO
+
+let folder = __SOURCE_DIRECTORY__
+let file = "datasamples\userprofiles-toptags.txt"
+
+let headers,observations =
+    let raw =
+        Path.Combine(folder, file)
+        |> File.ReadAllLines
+    
+    // first row is headers, first col is user ID
+    let headers = (raw.[0].Split ',').[1..]
+    
+    let observations =
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+
+    headers,observations
+
+// Listing 5-2. Basic dataset statistics
+printfn "%16s %8s %8s %8s" "Tag Name" "Avg" "Min" "Max"
+headers
+    |> Array.iteri (fun i name ->
+    let col = observations |> Array.map (fun obs -> obs.[i])
+    let avg = col |> Array.average
+    let min = col |> Array.min
+    let max = col |> Array.max
+    printfn "%16s %8.1f %8.1f %8.1f" name avg min max)
+
+// TODO: Listing 5-3. Plotting average usage by tag
