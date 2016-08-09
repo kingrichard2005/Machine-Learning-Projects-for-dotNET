13dd5143805c0a671892e744cd6fc8d439a09b0c
Rich <kingrichard2005@gmail.com>
2016-08-09 10:56:39 -0700

    * added sample from section "Running the Clustering Analysis"

--- Chapter5/Script.fsx
+++ Chapter5/Script.fsx
@@ -45,3 +45,35 @@ headers
     |> Chart.Bar
     |> fun chart -> chart.WithXAxis(LabelStyle=labels)
 
+
+//  Listing 5-7. Defining (Euclidean) distance and reduction for clustering
+#load "KMeans.fs"
+open Unsupervised.KMeans
+
+type Observation = float []
+let features = headers.Length
+
+//  Euclidean distance function
+let distance (obs1:Observation) (obs2:Observation) =
+    (obs1, obs2)
+    ||> Seq.map2 (fun u1 u2 -> pown (u1 - u2) 2)
+    |> Seq.sum
+
+let centroidOf (cluster:Observation seq) =
+    Array.init features (fun f ->
+        cluster
+        |> Seq.averageBy (fun user -> user.[f]))
+
+//  Listing 5-8. Example clustering the dataset
+// i.e. with an arbitrary value of '5' for k, i.e. the total number of clusters to derive
+let observations1 =
+    observations
+    |> Array.map (Array.map float)
+    |> Array.filter (fun x -> Array.sum x > 0.)
+
+let (clusters1, classifier1) =
+    let clustering = clusterize distance centroidOf
+    let k = 5
+    clustering observations1 k
+
+//  TODO: Leftoff on section "Analyzing the Results"
