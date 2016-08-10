2e03b4046c6818ad58f7b5b49444032b02d207c9
Rich <kingrichard2005@gmail.com>
2016-08-10 15:57:42 -0700

    * added examples from section "Rescaling Our Dataset to Improve Clusters"

--- Chapter5/Script.fsx
+++ Chapter5/Script.fsx
@@ -97,4 +97,26 @@ observations1
 |> Seq.iter (fun (clusterID, count) ->
     printfn "Cluster %i: %i elements" clusterID count)
 
-//  Leftoff: section "Rescaling Our Dataset to Improve Clusters"
+//  Rescaling Our Dataset to Improve Clusters
+let rowNormalizer (obs:Observation) : Observation =
+    let max = obs |> Seq.max
+    obs |> Array.map (fun tagUse -> tagUse / max)
+
+let observations2 =
+    observations
+    |> Array.filter (fun x -> Array.sum x > 0.)
+    |> Array.map (Array.map float)
+    |> Array.map rowNormalizer
+
+let (clusters2, classifier2) =
+    let clustering = clusterize distance centroidOf
+    let k = 5
+    clustering observations2 k
+
+observations2
+|> Seq.countBy (fun obs -> classifier2 obs)
+|> Seq.iter (fun (clusterID, count) ->
+    printfn "Cluster %i: %i elements" clusterID count)
+
+//  Leftoff on section "Identifying How Many Clusters to Search For"
+
