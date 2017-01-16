196bf6c9b2b5da79d609bec2fefbc4a0bc0d6f18
richard <richard@DESKTOP-5DT3A5F>
2017-01-15 20:27:47 -0800

    * added Chapter 6 dataset * started initial data exploration

---
+++ Chapter6_TreesAndForests/script.fsx
@@ -0,0 +1,16 @@
+﻿#r @"..\packages\FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+open FSharp.Data
+type Titanic = CsvProvider<"dataset\\titanic.csv">
+type Passenger = Titanic.Row
+let dataset = Titanic.GetSample ()
+
+dataset.Rows
+    |> Seq.countBy (fun passenger -> passenger.Survived)
+    |> Seq.iter (printfn "%A")
+
+dataset.Rows
+|> Seq.averageBy (fun passenger ->
+    if passenger.Survived then 1.0 else 0.0)
+|> printfn "Chances of survival: %.3f"
+
+// leftoff: Taking a Look at Features
