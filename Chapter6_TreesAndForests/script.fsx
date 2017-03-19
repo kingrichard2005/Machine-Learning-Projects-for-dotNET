239d786d050d7f2e219b94d0832d9d5080d214b2
kingrichard2005 <kingrichard2005@gmail.com>
2017-03-19 13:23:02 -0700

    * section examples

--- Chapter6_TreesAndForests/script.fsx
+++ Chapter6_TreesAndForests/script.fsx
@@ -13,7 +13,7 @@ dataset.Rows
     if passenger.Survived then 1.0 else 0.0)
 |> printfn "Chances of survival: %.3f"
 
-// leftoff: Taking a Look at Features
+// Taking a Look at Features
 // Listing 6-2. Computing the survival rate for different groups
 let survivalRate (passengers:Passenger seq) =
     let total = passengers |> Seq.length
@@ -22,3 +22,128 @@ let survivalRate (passengers:Passenger seq) =
         |> Seq.filter (fun p -> p.Survived)
         |> Seq.length
     100.0 * (float survivors / float total)
+
+let bySex =
+    dataset.Rows
+    |> Seq.groupBy(fun p -> p.Sex)
+
+bySex
+|> Seq.iter (fun (s,g) ->
+    printfn "Sex %A: %f" s (survivalRate g))
+
+let byClass =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Pclass)
+
+byClass
+|> Seq.iter (fun (s,g) ->
+    printfn "Class %A: %f" s (survivalRate g))
+
+// Listing 6-3. Learning a decision stump
+let mostFrequentLabelIn group =
+    group
+    |> Seq.countBy snd
+    |> Seq.maxBy snd
+    |> fst
+
+let learn sample extractFeature extractLabel =
+    // group together observations that have the
+    // same value for the selected feature, and
+    // find the most frequent label by group.
+    let groups =
+        sample
+        |> Seq.map (fun obs -> extractFeature obs, extractLabel obs)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (feat,group) -> feat, mostFrequentLabelIn group)
+
+    // for an observation, find the group with
+    // matching feature value, and predict the
+    // most frequent label for that group.
+    let classifier obs =
+        let featureValue = extractFeature obs
+        groups
+        |> Seq.find (fun (f,_) -> f = featureValue)
+        |> snd
+    classifier
+
+
+// Listing 6-4. Training and evaluating a stump
+let survived (p:Passenger) = p.Survived
+let sex (p:Passenger) = p.Sex
+let sexClassifier = survived |> learn (dataset.Rows) sex
+printfn "Stump: classify based on passenger sex."
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = sexClassifier p then 1.0 else 0.0)
+
+//Listing 6-5. A stump with multiple prongs
+let classClassifier = survived |> learn (dataset.Rows) (fun p -> p.Pclass)
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = classClassifier p then 1.0 else 0.0)
+
+
+//Listing 6-6. A feature with continuous numeric values
+let survivalByPricePaid =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Fare)
+    |> Seq.iter (fun (price,passengers) ->
+        printfn "%6.2F: %6.2f" price (survivalRate passengers))
+
+//Listing 6-7. Converting fare into discrete bins
+let averageFare =
+    dataset.Rows
+    |> Seq.averageBy (fun p -> p.Fare)
+
+let fareLevel (p:Passenger) =
+    if p.Fare < averageFare
+    then "Cheap"
+    else "Expensive"
+
+printfn "Stump: classify based on fare level."
+let fareClassifier = survived |> learn (dataset.Rows) fareLevel
+
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = fareClassifier p then 1.0 else 0.0)
+
+let survivalByPortOfOrigin =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Embarked)
+    |> Seq.iter (fun (port,passengers) ->
+        printfn "%s: %f" port (survivalRate passengers))
+
+//Listing 6-9. Incorporating missing values
+let hasData extractFeature = extractFeature >> Option.isSome
+
+let betterLearn sample extractFeature extractLabel =
+    let branches =
+        sample
+        |> Seq.filter (extractFeature |> hasData)
+        |> Seq.map (fun obs -> extractFeature obs |> Option.get, extractLabel obs)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (feat,group) -> feat, mostFrequentLabelIn group)
+        |> Map.ofSeq
+    let labelForMissingValues =
+        sample
+        |> Seq.countBy extractLabel
+        |> Seq.maxBy snd
+        |> fst
+    let classifier obs =
+        let featureValue = extractFeature obs
+        match featureValue with
+        | None -> labelForMissingValues
+        | Some(value) ->
+            match (branches.TryFind value) with
+            | None -> labelForMissingValues
+            | Some(predictedLabel) -> predictedLabel
+    classifier
+
+let port (p:Passenger) =
+    if p.Embarked = "" then None
+    else Some(p.Embarked)
+
+let updatedClassifier = survived |> betterLearn (dataset.Rows) port
+
+//leftoff: Measuring Information in Data
+
